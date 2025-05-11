1825df447866579822b2a225232803316a153a46
MIchowdhury <masum.iqbal.chowdhury@gmail.com>
2025-05-11 20:40:17 +0600

    I have made some changes to some files.

--- style-switcher.js
+++ style-switcher.js
@@ -1,4 +1,12 @@
+/* ====Toggler style switcher====== */
 const styleSwitcher = document.querySelector(".style-switcher-toggler");
 styleSwitcher.addEventListener("click", () => {
     document.querySelector(".style-switcher").classList.toggle("open");
+})
+
+// hide style - switcher on scroll
+window.addEventListener("scroll", () => {
+    if(document.querySelector(".style-switcher").classList.contains("open")){
+        document.querySelector(".style-switcher").classList.remove("open");
+    }
 })
